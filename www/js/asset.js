f986e764eb9b3b978ab657a3aa5f0af0a57447f3
Zoe Ames <zoeames@gmail.com>
2014-10-21 14:41:58 -0500

    asteroid images are randomly generated

--- www/js/asset.js
+++ www/js/asset.js
@@ -11,6 +11,13 @@ var Asset = (function(){
 
     asset.ship = new Image();
     asset.ship.src = 'img/assets/ship.png';
+    asset.asteroid1 = new Image();
+    asset.asteroid1.src = 'img/assets/asteroid1.jpg';
+    asset.asteroid2 = new Image();
+    asset.asteroid2.src = 'img/assets/asteroid2.jpg';
+    asset.asteroid3 = new Image();
+    asset.asteroid3.src = 'img/assets/asteroid3.jpg';
+
 
     return asset;
   };
--- www/js/rock.js
+++ www/js/rock.js
@@ -3,15 +3,19 @@ var Rock = (function(){
   'use strict';
 
   function Rock(line){
-    this.width = 40;
-    this.height = 40;
-    this.x = line.x;
-    this.y = line.y;
+    this.width        = 40;
+    this.height       = 40;
+    this.x            = line.x;
+    this.y            = line.y;
+    this.rockImgIndex = Math.floor((Math.random() * 3));
   }
 
   Rock.prototype.draw = function(game){
-    game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    var rockImgs     = [game.assets.asteroid1, game.assets.asteroid2, game.assets.asteroid3];
+    //game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    game.ctx.drawImage(rockImgs[this.rockImgIndex], this.x, this.y, this.width, this.height);
   };
+
 
   Rock.prototype.update = function(line){
     this.y = line.y;
